60258a3ce29635d47e01bfb7d2d460d3f4205959
Justin Reich <justinreich.dev@gmail.com>
2017-01-31 10:41:34 -0600

    Beginning implementation of controller method call in route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,39 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('posts.home');
 });
+
+// Route::get('/home', function () {
+//     return view('posts.home');
+// });
+
+Route::get('/home', 'PageController@getHomePage');
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/post/{postNumber}', function ($postNumber) {
+    $data = ['postNumber' => $postNumber];
+    return view('post.view-post', $data);
+});
+
+Route::get('/user-profile/{userNumber}', function ($userNumber) {
+    $data = ['userNumber' => $userNumber];
+    return view('users.user-profile', $data);
+});
+
+Route::get('/user-posts/{userNumber}', function ($userNumber) {
+    $data = ['userNumber' => $userNumber];
+    return view('users.user-posts', $data);
+});
+
+Route::get('/create-post', function () {
+    return view('posts.create-post');
+});
+
